92aac8b19839f563def396f552279c805241da6e
Amartya Mondal <atm1504.in@gmail.com>
2023-07-08 14:50:57 +0530

    Add randomizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,24 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeFace() {
+    setState(() {
+      leftDiceNumber = 1 + Random().nextInt(6);
+      rightDiceNumber = 1 + Random().nextInt(6);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,17 +41,20 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              onPressed: () {
+                changeFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Image.asset('images/dice1.png'),
-            ),
+            child: TextButton(
+                onPressed: () {
+                  changeFace();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
           ),
         ],
       ),
